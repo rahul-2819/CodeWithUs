284a598f5c456fa7d2667b3fb830621242e73adc
Rahul <rahul52045@gmail.com>
2024-04-23 19:46:55 +0530

    minor changes in workspace and alternative api for onecompiler api

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -6,113 +6,186 @@ function CodeEditor(props) {
   const [lang, setLang] = useState("cpp");
   const [res, setRes] = useState([]);
   const [testCases, setTestCases] = useState(null);
-  const RunAndCheck = async (Input_data) => {
-    console.log(Input_data);
-    const url = "https://onecompiler-apis.p.rapidapi.com/api/v1/run";
+
+  // -------------------------Judge0 compiler Apli------------------------
+  const RunAndCheck = async (inputData, expectedOutput) => {
+    const url = 'https://judge0-extra-ce.p.rapidapi.com/submissions?base64_encoded=false&wait=true&fields=*';
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "content-type": "application/json",
-        "X-RapidAPI-Key": "ac40a57194msh43667a7c72d4d16p15d807jsn1542329103f9",
-        "X-RapidAPI-Host": "onecompiler-apis.p.rapidapi.com",
+        'content-type': 'application/json',
+  //       'X-RapidAPI-Key': '2e08084134msh96ecd1e3924992cp1a8095jsndeb224acd56d',
+          'X-RapidAPI-Key': 'ac40a57194msh43667a7c72d4d16p15d807jsn1542329103f9',
+        'X-RapidAPI-Host': 'judge0-extra-ce.p.rapidapi.com'
       },
       body: JSON.stringify({
-        language: lang,
-        stdin: Input_data, // No input provided in this example
-        files: [
-          {
-            name: `code.${lang}`,
-            content: code,
-          },
-        ],
-      }),
+        language_id: 12,
+        source_code: code,
+        stdin: inputData
+      })
     };
+
     try {
       const response = await fetch(url, options);
       const result = await response.json();
-      console.log("res", result.stdout);
-      // setRes(result.stdout || result.stderr || "Unknown error occurred");
-      if (result.stdout) {
-        return result.stdout;
-      }
-      alert("error");
-      return result.stderr || "Unknown error";
-      // return result.stdout;
+
+      
+      return result.stdout === expectedOutput
     } catch (error) {
-      setRes("Error" + error.message);
+      console.error(error);
+      return "Error";
     }
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    props.handleLoading();
     const id = localStorage.getItem("CurrentQuestionName");
-    console.log(id);
+
     try {
       const response = await fetch(`http://localhost:5000/api/testcases/${id}`);
       const data = await response.json();
-      console.log(data.testCases);
       setTestCases(data.testCases);
-      // console.log(testCases);
     } catch (error) {
       console.log(error);
     }
+
     let answer = [];
     for (let key in testCases) {
-      // console.log(testCases[key].input_data)
-      const val = await RunAndCheck(testCases[key].input_data);
-      console.log(val);
-
-      if (val === testCases[key].expected_output) {
-        console.log(1);
-        answer.push(true);
-      } else {
-        console.log(0);
-        answer.push(false);
-      }
+      const testCase = testCases[key];
+      const val = await RunAndCheck(testCase.input_data, testCase.expected_output);
+      answer.push(val);
     }
     setRes(answer);
-
-    for (let key in res) {
-      if (res[key] === false) {
-      }
-    }
     props.updateOutput(answer);
   };
 
   const handleRun = async (e) => {
     e.preventDefault();
+    props.handleLoading();
     const id = localStorage.getItem("CurrentQuestionName");
-    console.log(id);
+
     try {
       const response = await fetch(`http://localhost:5000/api/exampletestcases/${id}`);
       const data = await response.json();
-      console.log(data.testCases);
       setTestCases(data.testCases);
-      // console.log(testCases);
     } catch (error) {
       console.log(error);
     }
+
     let answer = [];
     for (let key in testCases) {
-      // console.log(testCases[key].input_data)
-      const val = await RunAndCheck(testCases[key].input_data);
-      console.log(val);
-
-      if (val === testCases[key].expected_output) {
-        console.log(1);
-        answer.push(true);
-      } else {
-        console.log(0);
-        answer.push(false);
-      }
+      const testCase = testCases[key];
+      const val = await RunAndCheck(testCase.input_data, testCase.expected_output);
+      answer.push(val);
     }
     setRes(answer);
-
-    for (let key in res) {
-      if (res[key] === false) {
-      }
-    }
     props.updateOutput(answer);
   };
+
+  // const RunAndCheck = async (Input_data) => {
+  //   console.log(Input_data);
+  //   const url = "https://onecompiler-apis.p.rapidapi.com/api/v1/run";
+  //   const options = {
+  //     method: "POST",
+  //     headers: {
+  //       "content-type": "application/json",
+  //       "X-RapidAPI-Key": "ac40a57194msh43667a7c72d4d16p15d807jsn1542329103f9",
+  //       "X-RapidAPI-Host": "onecompiler-apis.p.rapidapi.com",
+  //     },
+  //     body: JSON.stringify({
+  //       language: lang,
+  //       stdin: Input_data, // No input provided in this example
+  //       files: [
+  //         {
+  //           name: `code.${lang}`,
+  //           content: code,
+  //         },
+  //       ],
+  //     }),
+  //   };
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const result = await response.json();
+  //     console.log("res", result.stdout);
+  //     // setRes(result.stdout || result.stderr || "Unknown error occurred");
+  //     if (result.stdout) {
+  //       return result.stdout;
+  //     }
+  //     alert("error");
+  //     return result.stderr || "Unknown error";
+  //     // return result.stdout;
+  //   } catch (error) {
+  //     setRes("Error" + error.message);
+  //     props.updateOutput("Error" + error.message);
+  //   }
+  // };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   props.handleLoading();
+  //   const id = localStorage.getItem("CurrentQuestionName");
+  //   console.log(id);
+  //   try {
+  //     const response = await fetch(`http://localhost:5000/api/testcases/${id}`);
+  //     const data = await response.json();
+  //     console.log(data.testCases);
+  //     setTestCases(data.testCases);
+  //     // console.log(testCases);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   let answer = [];
+  //   for (let key in testCases) {
+  //     // console.log(testCases[key].input_data)
+  //     const val = await RunAndCheck(testCases[key].input_data);
+  //     console.log(val);
+
+  //     if (val === testCases[key].expected_output) {
+  //       console.log(1);
+  //       answer.push(true);
+  //     } else {
+  //       console.log(0);
+  //       answer.push(false);
+  //     }
+  //   }
+  //   setRes(answer);
+
+  //   props.updateOutput(answer);
+  // };
+
+  // const handleRun = async (e) => {
+  //   e.preventDefault();
+  //   props.handleLoading();
+  //   const id = localStorage.getItem("CurrentQuestionName");
+  //   console.log(id);
+  //   try {
+  //     const response = await fetch(`http://localhost:5000/api/exampletestcases/${id}`);
+  //     const data = await response.json();
+  //     console.log(data.testCases);
+  //     setTestCases(data.testCases);
+  //     // console.log(testCases);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //   let answer = [];
+  //   for (let key in testCases) {
+  //     // console.log(testCases[key].input_data)
+  //     const val = await RunAndCheck(testCases[key].input_data);
+  //     console.log(val);
+
+  //     if (val === testCases[key].expected_output) {
+  //       console.log(1);
+  //       answer.push(true);
+  //     } else {
+  //       console.log(0);
+  //       answer.push(false);
+  //     }
+  //   }
+  //   setRes(answer);
+
+
+  //   props.updateOutput(answer);
+  // };
 
   useEffect(() => {
     // alert(res);
@@ -121,10 +194,6 @@ function CodeEditor(props) {
   return (
     <div className="container mx-auto p-6">
       <div className="flex justify-between items-center mb-4">
-        {/* <div>
-          <h1 className="text-3xl font-semibold"></h1>
-          <p className="text-gray-600"></p>
-        </div> */}
         <div className="flex items-center">
           <select
             className="border border-gray-400 px-4 py-2 rounded-md text-gray-800 hover:bg-gray-100 hover:border-gray-500 mr-4"
@@ -149,9 +218,11 @@ function CodeEditor(props) {
           </button>
         </div>
       </div>
-      <div className="w-full max-w-screen-lg mx-auto">
+      <div className="w-full max-w-screen-lg mx-auto"
+       style={{ maxHeight: "calc(80vh - 250px)", overflow: "hidden" }}
+       >
         <Editor
-          height="80vh"
+          height="100vh"
           theme="vs-dark"
           defaultLanguage={lang}
           defaultValue="// Start typing your code here..."
--- src/components/Output.js
+++ src/components/Output.js
@@ -1,7 +1,8 @@
-// Output.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-function Output({ res }) {
+function Output({ res, isLoading }) {
   const [expandedIndex, setExpandedIndex] = React.useState(null);
 
   const toggleOutput = (index) => {
@@ -17,35 +18,50 @@ function Output({ res }) {
   return (
     <div className="h-80 overflow-auto">
       <div className="border border-black-400 p-4">
-        {allCorrect ? (
-          <h2 className="text-xl font-semibold mb-4 text-green-600">Accepted</h2>
+        {isLoading ? (
+          <div className="flex items-center justify-center">
+            <FontAwesomeIcon icon={faSpinner} className="animate-spin mr-2" />
+            <span className="text-xl font-semibold">Executing...</span>
+          </div>
         ) : (
-          <h2 className="text-xl font-semibold mb-4 text-red-600">Wrong Answer</h2>
-        )}
-        <div className="flex flex-wrap">
-          {res.map((result, index) => (
-            <div key={index} className="w-1/4 px-2 mb-4">
-              <div
-                className={`border p-2 cursor-pointer ${expandedIndex === index ? 'bg-gray-200' : ''}`}
-                onClick={() => toggleOutput(index)}
-              >
-                <div className="flex items-center">
-                  <div className={`w-3 h-3 mr-2 rounded-full ${result ? 'bg-green-500' : 'bg-red-500'}`}></div>
-                  <div className={`font-semibold ${result ? 'text-green-600' : 'text-red-600'}`}>Test Case {index + 1}</div>
-                </div>
-                {expandedIndex === index && (
-                  <div className="mt-2">
-                    {result ? (
-                      <span className="text-green-600">Correct</span>
-                    ) : (
-                      <span className="text-red-600">Wrong Answer</span>
-                    )}
-                  </div>
+          <>
+            {res.length === 0 ? (
+              <h2 className="text-xl font-semibold mb-4">Click run to see your output</h2>
+            ) : (
+              <>
+                {allCorrect ? (
+                  <h2 className="text-xl font-semibold mb-4 text-green-600">Accepted</h2>
+                ) : (
+                  <h2 className="text-xl font-semibold mb-4 text-red-600">Wrong Answer</h2>
                 )}
-              </div>
-            </div>
-          ))}
-        </div>
+                <div className="flex flex-wrap">
+                  {res.map((result, index) => (
+                    <div key={index} className="w-1/4 px-2 mb-4">
+                      <div
+                        className={`border p-2 cursor-pointer ${expandedIndex === index ? 'bg-gray-200' : ''}`}
+                        onClick={() => toggleOutput(index)}
+                      >
+                        <div className="flex items-center">
+                          <div className={`w-3 h-3 mr-2 rounded-full ${result ? 'bg-green-500' : 'bg-red-500'}`}></div>
+                          <div className={`font-semibold ${result ? 'text-green-600' : 'text-red-600'}`}>Test Case {index + 1}</div>
+                        </div>
+                        {expandedIndex === index && (
+                          <div className="mt-2">
+                            {result ? (
+                              <span className="text-green-600">Correct</span>
+                            ) : (
+                              <span className="text-red-600">Wrong Answer</span>
+                            )}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
